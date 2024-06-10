e9bf6aa32f29601bf2b9794a07c8ad7fdd56eb82
fabianromeroclaros <fabian.romero.9385580@usalesiana.edu.bo>
2024-06-10 11:58:23 -0400

    feat: ast

---
+++ src/grammar/AST.hs
@@ -0,0 +1,93 @@
+module AST where
+
+-- Identifier and literals
+type Identifier = String
+
+data Literal
+    = IntLiteral Int
+    | BoolLiteral Bool
+    | DoubleLiteral Double
+    | StringLiteral String
+    deriving (Show, Eq)
+
+-- Data types
+data Type
+    = TInt
+    | TBool
+    | TDouble
+    | TString
+    deriving (Show, Eq)
+
+-- Expressions
+data Expr
+    = FunctionCall Identifier [Value]
+    | IfExpr ConditionExpr Expr Expr
+    | BinaryExpr Value ArithmeticOperator Value
+    | LambdaExpr [TypedIdentifier] Expr
+    deriving (Show, Eq)
+
+data Value
+    = VLiteral Literal
+    | VIdentifier Identifier
+    | VFunctionCall Identifier [Value]
+    deriving (Show, Eq)
+
+data ConditionExpr
+    = Condition Value ComparisonOperator Value
+    | ConditionAnd ConditionExpr ConditionExpr
+    | ConditionOr ConditionExpr ConditionExpr
+    | ConditionBool Bool
+    deriving (Show, Eq)
+
+-- Parameters and types
+data TypedIdentifier = TypedIdentifier Identifier Type
+    deriving (Show, Eq)
+
+-- Operators
+data ArithmeticOperator
+    = Add | Subtract | Multiply | Divide
+    deriving (Show, Eq)
+
+data ComparisonOperator
+    = LessThan | GreaterThan | LessEqual | GreaterEqual | Equal | NotEqual
+    deriving (Show, Eq)
+
+-- Pattern matching
+data Pattern
+    = PLiteral Literal
+    | PIdentifier Identifier
+    deriving (Show, Eq)
+
+data PatternMatch
+    = PatternMatch Pattern Expr
+    | Otherwise Expr
+    deriving (Show, Eq)
+
+-- Functions
+data Function = Function Identifier [TypedIdentifier] Type FunctionBody
+    deriving (Show, Eq)
+
+data FunctionBody
+    = FBExpr Expr
+    | FBPatternMatch [PatternMatch]
+    | FBLetStatement LetStatement
+    | FBComment Comment
+    deriving (Show, Eq)
+
+-- Let Statement
+data LetStatement = LetStatement TypedIdentifier Expr
+    deriving (Show, Eq)
+
+-- Comments
+type Comment = String
+
+-- Program (start)
+data Program = Program [ProgramElement]
+    deriving (Show, Eq)
+
+data ProgramElement
+    = PEFunction Function
+    | PELetStatement LetStatement
+    | PEComment Comment
+    | PEFunctionCall Expr
+    deriving (Show, Eq)
