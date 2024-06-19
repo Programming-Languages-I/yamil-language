fcde6e96db9af87aa40278f3b783ddd65b86eb9d
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-06-19 13:55:00 -0400

    test: add sample add unit tests for add function using HUnit

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,25 @@
+import Lib
+import Test.HUnit
+
+addPositivesTest :: Test
+addPositivesTest = TestCase (assertEqual "for (add 1 2)," 3 (add 1 2))
+
+addNegativesTest :: Test
+addNegativesTest = TestCase (assertEqual "for (add (-1) (-2))," (-3) (add (-1) (-2)))
+
+addPositiveAndNegativeTest :: Test
+addPositiveAndNegativeTest = TestCase (assertEqual "for (add 1 (-2))," (-1) (add 1 (-2)))
+
+addWithZeroTest :: Test
+addWithZeroTest = TestCase (assertEqual "for (add 0 5)," 5 (add 0 5))
+
+tests :: Test
+tests = TestList [
+          TestLabel "addPositives" addPositivesTest,
+          TestLabel "addNegativesTest" addNegativesTest,
+          TestLabel "addPositiveAndNegativeTest" addPositiveAndNegativeTest,
+          TestLabel "addWithZeroTest" addWithZeroTest
+        ]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = runTestTT tests >>= print
