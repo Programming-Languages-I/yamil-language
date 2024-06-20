b2c2d532cbda9339eec303a131564f71f0d60a0c
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-06-20 19:19:03 -0400

    feat(parser): implement parsing for `Pattern Matching` with `Guards`
    - feat(parsePatternMatch): add function for handling "->" operator
    - Imported necessary modules and defined parsers for pattern matching with guards

--- src/Parser/ParserPatternMatching.hs
+++ src/Parser/ParserPatternMatching.hs
@@ -1,9 +1,11 @@
 module Parser.ParserPatternMatching where
 
 import AST as AST
-import Parser.ParserValueTypes
+import Parser.ParserValueTypes 
+import Parser.ParserExpresions
+import Parser.LexerParser
 import Text.Parsec.String (Parser)
-import Text.Parsec ((<|>), try) 
+import Text.Parsec ((<|>), sepBy, try) 
 
 parsePattern :: Parser Pattern
 parsePattern = (try parsePatternLiteral) <|> parsePatternIdentifier
@@ -13,3 +15,12 @@ parsePatternLiteral = AST.PLiteral <$> parseLiteral
 
 parsePatternIdentifier :: Parser Pattern
 parsePatternIdentifier = AST.PIdentifier <$> parseIdentifier
+
+parsePatternMatches :: Parser [PatternMatch]
+parsePatternMatches = parsePatternMatchUsingGuards `sepBy` reservedOps "|"
+
+parsePatternMatchUsingGuards :: Parser PatternMatch
+parsePatternMatchUsingGuards = (try parsePatternMatch)
+
+parsePatternMatch :: Parser PatternMatch
+parsePatternMatch = AST.PatternMatch <$> parsePattern <* (whiteSpaces *> (reservedOps "->") <* whiteSpaces) <*> (parseLiteral)
