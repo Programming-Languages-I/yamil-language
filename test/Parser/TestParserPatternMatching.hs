5216393586a065ae44ff4284c9d53e90752b5ed9
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-06-20 19:37:58 -0400

    test: add unit test for `PatternMatching` with one `Guard`

---
+++ test/Parser/TestParserPatternMatching.hs
@@ -0,0 +1,16 @@
+module Parser.TestParserPatternMatching (module Parser.TestParserPatternMatching) where
+
+import AST
+import Parser.ParserPatternMatching
+import Test.Hspec
+import Text.Parsec
+
+testParsePatternMatching :: Spec
+testParsePatternMatching = describe "PatternMatchParser" $ do
+    it "parses a single match case" $ do
+        parse parsePatternMatches "" "0 -> \"Zero\"" `shouldBe` 
+                        Right [(PatternMatch (PLiteral (IntLiteral 0)) (StringLiteral "Zero"))]
+
+testParsePatternMatches :: Spec
+testParsePatternMatches = do 
+  testParsePatternMatching 
