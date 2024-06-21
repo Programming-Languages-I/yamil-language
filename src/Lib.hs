ec14df8b153d0a0570146fc3a27eb2303162b8ac
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-06-21 06:19:52 -0400

    refactor: update path code file

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,6 +7,6 @@ import Parser.ParserProgram as P
 
 someFunc :: IO ()
 someFunc = do
-    content <- readFile "src/resources/Example.yamil"
-    let result = parse (P.parseProgram) "src/resources/Example.yamil" content
+    content <- readFile "./app/resources/code.yamil"
+    let result = parse (P.parseProgram) "./app/resources/code.yamil" content
     print result
