35f763a69477e2c59b2cb97602fee43d4a3eb5cb
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-06-25 13:32:15 -0400

    feat(parser): implement parsing for `Pattern` data type

---
+++ src/Parser/ParserPatternMatching.hs
@@ -0,0 +1,15 @@
+module Parser.ParserPatternMatching where
+
+import AST as AST
+import Parser.ParserValueTypes 
+import Text.Parsec.String (Parser)
+import Text.Parsec ((<|>), try) 
+
+parsePattern :: Parser Pattern
+parsePattern = (try parsePatternLiteral) <|> parsePatternIdentifier
+
+parsePatternLiteral :: Parser Pattern
+parsePatternLiteral = AST.PLiteral <$> (try parseLiteral)
+
+parsePatternIdentifier :: Parser Pattern
+parsePatternIdentifier = AST.PIdentifier <$> (try parseIdentifier)
