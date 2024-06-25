0b05ee67ea96fe65b13dc1ebb4f8497aa54ae0d4
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-06-25 13:34:59 -0400

    feat(parser): implement parsing for `Pattern Matching` with `Guards`
    - feat(parsePatternMatchLit): add function for handling "->" operator
    - Imported necessary modules and defined parsers for pattern matching with guards

--- src/Parser/ParserPatternMatching.hs
+++ src/Parser/ParserPatternMatching.hs
@@ -2,6 +2,7 @@ module Parser.ParserPatternMatching where
 
 import AST as AST
 import Parser.ParserValueTypes 
+import Parser.LexerParser
 import Text.Parsec.String (Parser)
 import Text.Parsec ((<|>), try) 
 
@@ -13,3 +14,9 @@ parsePatternLiteral = AST.PLiteral <$> (try parseLiteral)
 
 parsePatternIdentifier :: Parser Pattern
 parsePatternIdentifier = AST.PIdentifier <$> (try parseIdentifier)
+
+parsePatternMatchUsingGuards :: Parser PatternMatch
+parsePatternMatchUsingGuards = (try parsePatternMatchLit)
+
+parsePatternMatchLit :: Parser PatternMatch
+parsePatternMatchLit = AST.PatternMatchLit <$> (whiteSpaces *> parsePattern) <*> (whiteSpaces *> reservedOps "->" *> whiteSpaces *> parseLiteral)
