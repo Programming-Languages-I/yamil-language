74419aaa0edf6676d7bfcc494b7016044aab72ec
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-07-04 00:13:41 -0400

    feat: update output code file generated by interpreter

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,5 +13,5 @@ someFunc = do
         let result = parse (P.parseProgram) "./app/resources/code.yamil" content
         print result
 
-        interpreter <- (CT.writePascalFile) "./app/resources/code.pas" exampleLiterals
+        interpreter <- (CT.writePascalFile) "./app/resources/code.pas" exampleVars exampleLiterals
         print interpreter
