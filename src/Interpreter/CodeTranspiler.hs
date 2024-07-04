025e63d0037b4d68d38360605e36c98ee7ca63eb
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-07-03 22:43:41 -0400

    feat: implement interpreter for literals

---
+++ src/Interpreter/CodeTranspiler.hs
@@ -0,0 +1,30 @@
+module Interpreter.CodeTranspiler (writePascalFile, exampleLiterals) where
+
+import AST
+import Data.Text.Prettyprint.Doc
+import Prettyprinter.Render.String (renderString)
+
+literalToPascal :: Literal -> Doc ann
+literalToPascal (IntLiteral n)    = pretty n
+-- literalToPascal (BoolLiteral b)   = if b then "True" else "False"
+literalToPascal (DoubleLiteral d) = pretty d
+literalToPascal (StringLiteral s) = dquotes $ pretty s
+
+literalsToPascal :: [Literal] -> Doc ann
+literalsToPascal literals = vsep (map literalToPascal literals)
+
+generatePascalProgram :: [Literal] -> Doc ann
+generatePascalProgram literals =
+  vsep [ pretty "program Yamil;"
+       , pretty "begin"
+       , indent 2 (literalsToPascal literals)
+       , pretty "end." 
+       ]
+
+exampleLiterals :: [Literal] 
+exampleLiterals = [IntLiteral 42, DoubleLiteral 3.14, StringLiteral "Hello, Pascal!"]
+
+writePascalFile :: FilePath -> [Literal] -> IO ()
+writePascalFile filePath literals = do
+    let pascalCode = renderString $ layoutPretty defaultLayoutOptions $ generatePascalProgram literals
+    writeFile filePath pascalCode        
